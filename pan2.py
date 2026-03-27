19464444ec4e7cfcde87b16d5621f80a4e484532
Sameera-2007 <24331a05f7@mvgrce.edu.in>
2026-03-28 00:24:42 +0530

    Add files via upload

---
+++ pan2.py
@@ -0,0 +1,7 @@
+import pandas as pd
+df = pd.read_csv("data.csv")
+sorted_df = df.sort_values(by="Name", ascending=True)
+print("Sorted Data:\n", sorted_df)
+print("\nFirst 3 rows:\n", df.iloc[:3])
+print("\nSelected Columns:\n", df[["Name", "Marks"]])
+
